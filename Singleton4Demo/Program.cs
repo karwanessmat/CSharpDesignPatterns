2eadaa50b501afb92668194422dc315f7d9be380
Karwan Essmat Othman <karwan.essmat@outlook.com>
2024-04-06 17:54:48 +0300

    Singleton4Demo was added

---
+++ Singleton4Demo/Program.cs
@@ -0,0 +1,6 @@
+﻿using Singleton4Demo;
+
+var appSettings = AppConfigurations.Instance;
+Console.WriteLine(appSettings.GetSetting("ConnectionString"));
+Console.WriteLine(appSettings.GetSetting("ApiKeyName"));
+// Use connectionString and apiKey in your application
