26252eb68bae32e9e52982b5a86e471795f2bafd
Karwan Essmat Othman <karwan.essmat@outlook.com>
2024-04-09 16:01:36 +0300

    updated

--- Proxy1Demo/Proxy.cs
+++ Proxy1Demo/Proxy.cs
@@ -3,15 +3,8 @@
 namespace Proxy1Demo
 {
     // The Proxy has an interface identical to the RealSubject.
-    internal class Proxy : ISubject
+    internal class Proxy(RealSubject realSubject) : ISubject
     {
-        private readonly RealSubject _realSubject;
-
-        public Proxy(RealSubject realSubject)
-        {
-            this._realSubject = realSubject;
-        }
-
         // The most common applications of the Proxy pattern are lazy loading,
         // caching, controlling the access, logging, etc. A Proxy can perform
         // one of these things and then, depending on the result, pass the
@@ -19,7 +12,7 @@ namespace Proxy1Demo
         public void Request()
         {
             if (!CheckAccess()) return;
-            _realSubject.Request();
+            realSubject.Request();
 
             LogAccess();
         }
